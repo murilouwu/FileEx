bc370958b40b742476600cccf5e4a0b176337f3d
Murilo Gimenez <91744500+murilouwu@users.noreply.github.com>
2023-05-08 12:29:00 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/39cab4bf95.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.js" integrity="sha256-tA8y0XqiwnpwmOIl3SGAcFl2RvxHjA8qp0+1uCGmRmg=" crossorigin="anonymous"></script>  
+    <link rel="stylesheet" href="css.css">
+    <title>File Explore</title>
+</head>
+<body>
+    <div class="legenda">
+        <div class="ul"><i class="icon fa-solid fa-file"></i> Arquivo</div>
+        <div class="ul"><i class="icon fa-solid fa-file-image"></i> Imagem</div>
+        <div class="ul"><i class="icon fa-solid fa-film"></i> Video</div>
+        <div class="ul"><i class="icon fa-solid fa-volume-off"></i> Audio</div>
+        <div class="ul"><i class="icon fa-solid fa-folder"></i> Pasta</div>
+    </div>
+    <div class="tolls">
+        <div class="toll"><i class="icon fa-solid fa-plus"></i></div>
+        <div class="toll"><i class="icon fa-solid fa-trash"></i></div>
+        <div class="toll"><i class="icon fa-solid fa-folder-plus"></i></div>
+    </div>
+    <div class="painel">
+        <div class="linha">
+            <input type="checkbox" id="NameFile">
+            <i class="icon fa-solid fa-file"></i>
+            <label> NameFile </label>
+            <button class="btn">Excluir</button>
+            <button class="btn">Renomear</button>
+            <button class="btn">Abrir</button>
+            <button class="btn">Clonar</button>
+        </div>
+    </div>
+    <?php
+    
+    ?>
+</body>
+</html>
